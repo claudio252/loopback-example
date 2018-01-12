f02180ed9e57c311da26499155770473346c8426
Claudio A. Cuellar Valencia <claudio.cuellar@gmail.com>
2018-01-12 16:08:31 -0400

    Ejemplo de control de accesos

--- server/models/donante.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(Donante) {
-  Donante.beforeRemote('create', function(context, donante, next) {
-    console.log('Nuevo donante con nombre: ', context.req.body.name);
-
-    next();
-  });
-};
